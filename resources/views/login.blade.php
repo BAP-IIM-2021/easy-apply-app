cdabfff83abfd52e7d79b7f1fcd602ab08b5fc67
“Noro” <thomas.aubert@edu.devinci.fr>
2021-11-19 10:58:49 +0100

    :tada: Setup Project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <section>
+        <h1>Inscrivez-vous</h1>
+        <form action="POST">
+            <input type="text" placeholder="Nom">
+            <input type="text" placeholder="Prénom">
+            <input type="date" placeholder="Date de Naissance">
+            <input type="text" placeholder="Ville">
+            <input type="email" placeholder="Adresse mail">
+            <input type="tel" placeholder="Numéro">
+            <input type="password" placeholder="Mot de passe">
+        </form>
+    </section>
+</body>
+</html>

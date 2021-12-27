b7e5d550637505b7a5ca6235c5f5e7c61fd4350b
“Noro” <thomas.aubert@edu.devinci.fr>
2021-12-27 17:41:19 +0100

    :hammer: Add Softs skills choice at Inscription & buttons add/delete JS

--- public/js/app.js
+++ public/js/app.js
@@ -2074,6 +2074,49 @@ document.getElementById('back_inscription').addEventListener('click', function (
   document.getElementById('submit_inscription').style.display = "none";
   document.getElementById('next_inscription').style.display = "block";
 });
+var delete_ss = document.getElementById('dlt_ss');
+var add_ss = document.getElementById('add_ss'); // Add Soft Skills
+
+add_ss.addEventListener('click', function () {
+  delete_ss.disabled = false; // Enable button delete
+
+  delete_ss.style.filter = 'none'; // Remove grayscale
+
+  var clone = document.getElementById('ss').cloneNode(true); // Clone div Soft skills
+
+  document.getElementById('ss_content').appendChild(clone); // Add the clone as a child of the parent 'ss_content'
+
+  var count = document.getElementById('ss_content').getElementsByTagName('div').length; // Calculate the number of div in the parent 'ss_content'
+
+  if (count > 1) {
+    // If there are more than 1 div, display the Delete button
+    delete_ss.style.display = 'flex';
+
+    if (count >= 5) {
+      // If there are 5 or more div, disable button add (so there are 5 options max) & add grayscale
+      add_ss.disabled = true;
+      add_ss.style.filter = "grayscale(100%)";
+    }
+  }
+}); // Delete Soft Skills
+
+delete_ss.addEventListener('click', function () {
+  var count = document.getElementById('ss_content').getElementsByTagName('div').length;
+  add_ss.disabled = false; // Enable button Add
+
+  add_ss.style.filter = 'none'; // Remove grayscale 
+
+  if (count > 1) {
+    var parent = document.getElementById('ss_content');
+    parent.removeChild(parent.lastChild); // Remove the last child of  the parent 'ss_content'
+
+    if (count == 2) {
+      // If there are 1 div (2 because it will refresh when user click), disable button delete (so there is alway one div) & add grayscale
+      delete_ss.disabled = true;
+      delete_ss.style.filter = "grayscale(100%)";
+    }
+  }
+}); // .appendChild(document.getElementById('ss_content'));
 
 /***/ }),
 
--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -52,7 +52,7 @@
             </div>
 
 
-            <div id="content_inscription_2" class="justify-center items-center h-xl hidden bg">
+            <div id="content_inscription_2" class="justify-center items-center hidden bg">
                 <div class="flex flex-wrap" >
                     <div class="flex flex-col items-start mx-16 my-4">
                         <label for="website"class="my-2" >Site Internet</label>
@@ -70,28 +70,89 @@
                             <option value="2">2</option>
                         </select>
                     </div>
-                    <div class="flex flex-col items-start mx-16 my-4">
-                        <label for="soft_skills" class="my-2">Soft Skills</label>
-                        <select class="btn-primary" type="text" placeholder="Site" name="soft_skills"> 
-                            <option value="">--Sélectionnez l'option--</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                        </select>
+                    <div id="ss_container" class=" mx-16 mt-4">
+                        <label for="soft_skills">Soft Skills</label>
+                        <div id="ss_content">
+                            <div id="ss" class="flex flex-col items-start mt-4">
+                                <select class="btn-primary" type="text" placeholder="Site" name="soft_skills"> 
+                                    <option value="">--Sélectionnez l'option--</option>
+                                    <option value="">La prise de décision</option>
+                                    <option value="">La délégation</option>
+                                    <option value="">La facilitation</option>
+                                    <option value="">La supervision</option>
+                                    <option value="">Savoir écouter</option>
+                                    <option value="">Convaincre</option>
+                                    <option value="">Persuader</option>
+                                    <option value="">Négocier</option>
+                                    <option value="">Argumenter</option>
+                                    <option value="">Présenter</option>
+                                    <option value="">L'audace</option>
+                                    <option value="">L'autonomie</option>
+                                    <option value="">La curiosité</option>
+                                    <option value="">La mémoire</option>
+                                    <option value="">Le respect</option>
+                                    <option value="">La respectabilité</option>
+                                    <option value="">La présentation</option>
+                                    <option value="">L'intelligence culturelle</option>
+                                    <option value="">La présence</option>
+                                    <option value="">La conscience de soi</option>
+                                    <option value="">L'introspection</option>
+                                    <option value="">La maîtrise de soi</option>
+                                    <option value="">L'observation critique</option>
+                                    <option value="">La pensée critique</option>
+                                    <option value="">La pensée logique</option>
+                                    <option value="">La rigueur</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="flex w-tiny justify-end text-white">
+                            <input value="Supprimer -" type="button" id="dlt_ss" class="cursor-pointer grayscale shadow-md m-2 px-4 py-2 rounded-lg bg-light-blue">
+                            <input value="Ajouter +" type="button" id="add_ss" class="cursor-pointer shadow-md m-2 px-4 py-2 rounded-lg bg-light-blue">
+                            
+                        </div>
                     </div>
                     <div class="flex flex-col items-start mx-16 my-4">
                         <label for="experience_years" class="my-2">Année d'expériences</label>
                         <select class="btn-primary" type="text" placeholder="Site" name="experience_years"> 
                             <option value="">--Sélectionnez l'option--</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
+                            
                         </select>
                     </div>
                     <div class="flex flex-col items-start mx-16 my-4">
                         <label for="formation" class="my-2">Formation</label>
                         <select class="btn-primary" type="text" placeholder="Site" name="formation"> 
                             <option value="">--Sélectionnez l'option--</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
+                            <option value="">Agriculture</option>
+                            <option value="">Animation</option>
+                            <option value="">Animation 2D/3D</option>
+                            <option value="">Art</option>
+                            <option value="">Beauté</option>
+                            <option value="">Bien-être</option>
+                            <option value="">Commerce</option>
+                            <option value="">Communication</option>
+                            <option value="">Comptabilité</option>
+                            <option value="">Culture</option>
+                            <option value="">Design</option>
+                            <option value="">Droit</option>
+                            <option value="">Environnement</option>
+                            <option value="">Finance</option>
+                            <option value="">Fonction publique</option>
+                            <option value="">Hôtellerie</option>
+                            <option value="">Image</option>
+                            <option value="">Informatique</option>
+                            <option value="">Ingénierie</option>
+                            <option value="">Langues</option>
+                            <option value="">Management</option>
+                            <option value="">Marketing</option>
+                            <option value="">Mode</option>
+                            <option value="">Santé</option>
+                            <option value="">Sciences</option>
+                            <option value="">Sciences Humaines</option>
+                            <option value="">Sciences Politiques</option>
+                            <option value="">Sport</option>
+                            <option value="">Social</option>
+                            <option value="">Son</option>
+                            <option value="">Tourisme</option>
                         </select>
                     </div>
                     <div class="flex flex-col items-start mx-16 my-4">

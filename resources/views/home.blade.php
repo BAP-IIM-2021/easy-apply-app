f091da47fe9f0bcaf6e56c23389a4ee4c40021f3
Guillaume <guillaume.fine@edu.devinci.fr>
2021-12-28 15:59:29 +0100

    :wrench: Update Tailwind config, :lipstick: Update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,107 +2,102 @@
 @include('partials/header')
 
 @section('content')
-
-
-
 <div class="bg-light-blue block text-white p-24 pb-48">
-    <h1 class="text-5xl">Et si on postulait autrement ?</h1><br>
-    <h3 class="text-3xl font-bold py-2">Favorisons la rencontre entre l'offre et la demande d'emploi</h3>
-    <p class="text-white">Optimisez votre recrutement à moindre coût grâce à notre service tout en un : publications illimitées d’offres, matching et scoring des</p>
-    <p class="text-white">candidats, gestion automatisé des candidatures.</p>
+  <h1 class="text-5xl">Et si on postulait autrement ?</h1><br>
+  <h3 class="text-3xl font-bold py-2">Favorisons la rencontre entre l'offre et la demande d'emploi</h3>
+  <p class="text-white">Optimisez votre recrutement à moindre coût grâce à notre service tout en un : publications illimitées d’offres, matching et scoring des</p>
+  <p class="text-white">candidats, gestion automatisé des candidatures.</p>
 </div>
 
 <article class="mt-36">
-    <h2 class="text-center text-6xl mb-32"> Qui sommes-nous ?</h2>
+  <h2 class="text-center text-6xl mb-32"> Qui sommes-nous ?</h2>
 
-    <div class="flex my-5 justify-evenly">
-        <div class="ml-24">
-            <iframe width="750" class="h-full"
-                    src="https://www.youtube.com/watch?v=rcWzZz969qg&list=PLFHz2csJcgk8lgiRDB7FdsXVr4xy6jE8K&index=3">
-            </iframe>
-        </div>
-        <div class="shadow-md bg-white rounded-lg ml-24 mr-24 p-10 relative">
-            <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-44 h-44 m-auto">
-                <img width="170" height="170" class="m-auto" src="./img/logo.png" alt="logo">
-            </div>
-
-            <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
-            <p class="text-center">Application de recrutement pas système de matching</p>
-
-            <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
-            <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
-            <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
-            <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
-            <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
-        </div>
+  <div class="flex my-5 justify-evenly">
+    <div class="ml-24">
+      <iframe width="750" class="h-full" src="https://www.youtube.com/watch?v=rcWzZz969qg&list=PLFHz2csJcgk8lgiRDB7FdsXVr4xy6jE8K&index=3"></iframe>
     </div>
+    <div class="shadow-md bg-white rounded-lg ml-24 mr-24 p-10 relative">
+      <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-44 h-44 m-auto">
+        <img width="170" height="170" class="m-auto" src="./img/logo.png" alt="logo">
+      </div>
+
+      <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+      <p class="text-center">Application de recrutement pas système de matching</p>
+
+      <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
+      <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
+      <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
+      <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
+      <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+    </div>
+  </div>
 </article>
 
 <section class="flex mt-24">
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
-        <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
-            <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
-        </div>
-
-        <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
-        <p class="text-center">Application de recrutement pas système de matching</p>
-
-        <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
-        <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
-        <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
-        <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
-        <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
+      <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
-        <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
-            <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
-        </div>
+    <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+    <p class="text-center">Application de recrutement pas système de matching</p>
 
-        <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
-        <p class="text-center">Application de recrutement pas système de matching</p>
+    <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
+    <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
+    <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
+    <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
+    <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  </div>
 
-        <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
-        <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
-        <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
-        <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
-        <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
+      <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
-        <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
-            <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
-        </div>
+    <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+    <p class="text-center">Application de recrutement pas système de matching</p>
 
-        <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
-        <p class="text-center">Application de recrutement pas système de matching</p>
+    <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
+    <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
+    <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
+    <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
+    <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  </div>
 
-        <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
-        <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
-        <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
-        <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
-        <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
+      <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
-        <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
-            <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
-        </div>
+    <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+    <p class="text-center">Application de recrutement pas système de matching</p>
 
-        <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
-        <p class="text-center">Application de recrutement pas système de matching</p>
+    <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
+    <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
+    <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
+    <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
+    <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  </div>
 
-        <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
-        <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
-        <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
-        <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
-        <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-16 w-24 h-24 m-auto">
+      <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
     </div>
+
+    <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+    <p class="text-center">Application de recrutement pas système de matching</p>
+
+    <h4 class="text-light-blue text-2xl mt-14">Compétences :</h4>
+    <p class="mt-5"><span class="font-bold">Offres illimités :</span> les publications d'offres d'emploi sont illimitées</p>
+    <p class="mt-5"><span class="font-bold">Visibilité de l'offre :</span> votre offre est proposée aux profils que vous cherchez et à ceux intéressés par vos mission</p>
+    <p class="mt-5"><span class="font-bold">Gain de temps :</span> les candidats sont classées automatiquement selon vos critères.</p>
+    <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
+  </div>
 </section>
 
 <article class="bg-light-blue text-white m-36 p-8 h-80 mt-40">
-    <h3 class="text-3xl mx-10 my-5">Vous aimeriez voir votre entreprise sur notre site ?</h3>
-    <p class="font-bold text-2xl text-center mt-20">Créez votre page d'entreprise en appuyant simplement ici !</p><br>
-    <p  class="text-center"><a href="" class="rounded-full bg-white text-black py-2 px-4 text-center">Page d'entreprise</a></p>
+  <h3 class="text-3xl mx-10 my-5">Vous aimeriez voir votre entreprise sur notre site ?</h3>
+  <p class="font-bold text-2xl text-center mt-20">Créez votre page d'entreprise en appuyant simplement ici !</p><br>
+  <p class="text-center text-light-black"><a href="" class="rounded-full bg-white  py-2 px-4 text-center">Page d'entreprise</a></p>
 </article>
 @endsection

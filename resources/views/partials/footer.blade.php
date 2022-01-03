056ef44e3bb160f7e45ddf3c872a5e55bc81a12d
YesIDo49 <mehdialsidcheikh@gmail.com>
2022-01-03 13:47:03 +0100

    :lipstick: add underline animation

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -14,18 +14,20 @@
         </div>
         <div class=" flex flex-col justify-center text-center xl:text-left">
             <h3 class="font-bold text-2xl mb-10 ">S'inscrire</h3>
-            <a href="" class="text-xl mb-6">En temps qu'entreprise</a>
-            <a href="{{ route ('inscription') }}" class="text-xl mb-6">En temps que chercheur d'emploi</a>
+            <a href="" class="text-xl mb-6 font-display max-w-sm leading-tight">
+                <span class="link link-underline">En temps qu'entreprise</span></a>
+            <a href="{{ route ('inscription') }}" class="text-xl mb-6 font-display max-w-sm leading-tight">
+                <span class="link link-underline">En temps que chercheur d'emploi</span></a>
         </div>
     </div>
     <div class="flex justify-center">
-        <a class="mx-8 hover:text-light-blue" href="">
+        <a class="mx-8 transition duration-150 ease-out hover:ease-in hover:text-light-blue" href="">
             <i class="fab fa-instagram fa-3x"></i>
         </a>
-        <a class="mx-8 hover:text-light-blue" href="">
+        <a class="mx-8 transition duration-150 ease-out hover:ease-in hover:text-light-blue" href="">
         <i class="fab fa-linkedin fa-3x"></i>
         </a>
-        <a class="mx-8 hover:text-light-blue" href="">
+        <a class="mx-8 transition duration-150 ease-out hover:ease-in hover:text-light-blue" href="">
             <i class="fab fa-facebook-square fa-3x"></i>
         </a>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
   </div>
     <ul class="flex justify-evenly w-1/2 xl:w-1/3 items-center">
       <li>
-        <a href="" class="text-xl hover:text-light-blue transition duration-150 ease-out hover:ease-in">Accueil</a>
+        <a href="" class="text-xl hover:text-light-blue transition duration-300 ease-out hover:ease-in">Accueil</a>
       </li>
       <li>
         <a href="{{ route('login') }}" class=" w-sm px-6 py-4 bg-light-blue text-white rounded-2xl shadow-md hover:bg-blue-500 transition duration-150 ease-out hover:ease-in focus:outline-none focus:ring-2 cursor-pointer text-center">Se connecter</a>

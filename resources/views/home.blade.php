21022053efd0a68f04756eabcba0e12aa329de6a
YesIDo49 <mehdialsidcheikh@gmail.com>
2022-01-03 10:35:35 +0100

    :iphone: add responsive to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,28 @@
 @include('partials/header')
 
 @section('content')
-  <div class="bg-primary block text-white p-24 pb-48">
-    <h1 class="text-5xl">Et si on postulait autrement ?</h1><br>
-    <h3 class="text-3xl font-bold py-2">Favorisons la rencontre entre l'offre et la demande d'emploi</h3>
-    <p class="text-white">Optimisez votre recrutement à moindre coût grâce à notre service tout en un : publications illimitées d’offres, matching et scoring des</p>
-    <p class="text-white">candidats, gestion automatisé des candidatures.</p>
+
+
+  <div class="bg-primary block text-white p-24 xl:p-24 xl:pb-48 text-center xl:text-left">
+    <h1 class="text-5xl my-5">Et si on postulait autrement ?</h1><br>
+    <h3 class="text-3xl font-bold my-10 xl:my-3">Favorisons la rencontre entre l'offre et la demande d'emploi</h3>
+    <p class="text-white text-xl">Optimisez votre recrutement à moindre coût grâce à notre service tout en un : publications illimitées d’offres, matching et scoring des</p>
+    <p class="text-white text-xl">candidats, gestion automatisé des candidatures.</p>
   </div>
 
-  <article class="mt-36 ">
-    <h2 class="text-center text-6xl mb-32">Qui sommes-nous ?</h2>
+  <article class="my-36 ">
+    <h2 class="text-center text-6xl xl:mb-32  hover:text-light-blue transition duration-300 ease-out hover:ease-in">Qui sommes-nous ?</h2>
 
-    <div class="flex my-5 justify-evenly">
-      <div class="ml-24">
-        <iframe width="750" class="h-full" src="https://www.youtube.com/watch?v=rcWzZz969qg&list=PLFHz2csJcgk8lgiRDB7FdsXVr4xy6jE8K&index=3"></iframe>
+    <div class="flex flex-col justify-center place-items-center my-12 xl:justify-evenly xl:flex-row">
+      <div class="flex justify-center my-14 ">
+        <iframe width="500" class="p-2 h-full xl:p-0" src="https://www.youtube.com/watch?v=rcWzZz969qg&list=PLFHz2csJcgk8lgiRDB7FdsXVr4xy6jE8K&index=3"></iframe>
       </div>
-      <div class="shadow-md bg-white rounded-lg ml-24 mr-24 p-10 relative">
-        <div class="flex rounded-full bg-white shadow-md absolute translate-x-1/2 left-1/3 -top-24 w-sm h-sm ">
+      <div class="shadow-md bg-white rounded-lg px-10 py-24 relative m-14">
+        <div class="flex justify-center place-items-center rounded-full bg-white shadow-md absolute xl:translate-x-1/3 left-48 xl:left-1/3 -top-24 w-sm h-sm ">
           <img width="170" height="170" class="m-auto" src="./img/logo.png" alt="logo">
         </div>
 
-        <h3 class="uppercase text-center text-2xl font-bold mt-16">Easy Apply</h3>
+        <h3 class="uppercase text-center text-3xl font-bold mt-12 ">Easy Apply</h3>
         <p class="text-center">Application de recrutement pas système de matching</p>
 
         <h4 class="text-primary text-2xl mt-14">Compétences :</h4>
@@ -33,8 +35,8 @@
     </div>
   </article>
 
-  <section class="flex mt-24">
-    <div class="card shadow-md bg-white rounded-lg p-10 m-5 relative">
+  <section class="xl:flex xl:flex-row xl:justify-evenly xl:mt-24 flex flex-col justify-center place-items-center">
+    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative w-lg my-14 ">
       <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-sm h-sm">
         <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
       </div>
@@ -49,7 +51,7 @@
       <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative w-lg my-14">
       <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-sm h-sm">
         <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
       </div>
@@ -64,7 +66,7 @@
       <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative w-lg my-14">
       <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-sm h-sm">
         <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
       </div>
@@ -79,7 +81,7 @@
       <p class="mt-5"><span class="font-bold">Économies :</span> un tarif annuel et unique pour l'utilisation complète et illimitée de tous nos services</p>
     </div>
 
-    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative">
+    <div class="shadow-md bg-white rounded-lg p-10 m-5 relative w-lg my-14">
       <div class="flex rounded-full bg-white shadow-md absolute left-1/3 -top-24 w-sm h-sm">
         <img width="100" height="100" class="m-auto" src="./img/logo.png" alt="logo">
       </div>
@@ -95,14 +97,11 @@
     </div>
   </section>
 
-  <article class="bg-primary text-white m-36 p-10 flex flex-col justify-center place-items-center gap-8">
-    <h3 class="text-4xl">Vous aimeriez voir votre entreprise sur notre site ?</h3>
-    <p class="font-bold text-2xl">Créez votre page d'entreprise en appuyant simplement ici !</p><br>
-    <a href="./job-created.blade.php" class="rounded-full border-2 border-white bg-white text-xl text-primary py-2 px-4 hover:text-white hover:bg-primary hover:border-2 hover:border-white transition duration-150 ease-out hover:ease-in">Page d'entreprise</a>
+  <article class="bg-primary text-white text-center m-24 xl:m-36 p-10 flex flex-col justify-center place-items-center gap-8">
+    <h3 class="xl:text-4xl text-2xl">Vous aimeriez voir votre entreprise sur notre site ?</h3>
+    <p class="font-bold xl:text-2xl text-xl">Créez votre page d'entreprise en appuyant simplement ici !</p><br>
+    <a href="" class="rounded-full border-2 border-white bg-white text-xl text-primary py-2 px-4 hover:text-white hover:bg-primary hover:border-2 hover:border-white transition duration-150 ease-out hover:ease-in">Page d'entreprise</a>
   </article>
 
-  <div class="test">
-    <h1>test</h1>
-  </div>
 @endsection
 

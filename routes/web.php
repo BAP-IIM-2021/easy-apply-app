f64274adac21a95e5f0cc60a35f2006ea35e1458
Guillaume <guillaume.fine@edu.devinci.fr>
2022-01-04 09:49:41 +0100

    :hammer: Update routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\RegisterController;

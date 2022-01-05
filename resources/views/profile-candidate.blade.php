aaa9c819ae1e6be7c2180c006bd12fb68ab4c3b9
YesIDo49 <mehdialsidcheikh@gmail.com>
2022-01-05 11:05:41 +0100

    :lipstick: update candidate profile + add new animation for input files

--- resources/views/profile-candidate.blade.php
+++ resources/views/profile-candidate.blade.php
@@ -4,12 +4,15 @@
 @section('content')
 
 
-    <section class="bg-gray flex justify-center h-100 ">
-        <img src="../img/add-pic.svg" alt="add pic" class="my-36">
+    <section class="bg-gray flex justify-center h-100 relative">
+        <img src="../img/add-pic.svg" alt="add pic" class="my-60">
+        <div class="flex rounded-full bg-white shadow-md absolute left-[10%] bottom-[-40%] w-lg h-lg">
+            <a href="" class="m-auto"><img width="50" height="50"  src="../img/add-pic.svg" alt="logo"></a>
+        </div>
     </section>
 
-    <article class="flex flex-col justify-evenly m-14 place-items-center xl:flex xl:flex-row xl:justify-around">
-        <div class="flex flex-col justify-between place-items-center">
+    <article class="flex flex-col justify-evenly mx-14 mt-56 mb-20 place-items-center xl:flex xl:flex-row xl:justify-around xl:text-left ">
+        <div class="flex flex-col justify-between place-items-center ">
 
             <div class="flex flex-row my-10">
                 <h2 class="font-title text-4xl mx-10">Clémentine PUNTELLE, 28/06/1983</h2>
@@ -20,16 +23,17 @@
 
         </div>
 
-        <div class="flex flex-col justify-center my-10">
-            <img src="../img/logo.png" alt="overwrite" class="w-sm h-sm">
-            <h3 class="text-3xl">Université de Nantes</h3>
+        <div class="flex flex-row justify-center my-10">
+            <img src="../img/u-nantes.svg" alt="logo université" width="50" class="mx-4">
+            <h3 class="text-3xl flex flex-col justify-center">Université de Nantes</h3>
         </div>
+
 
 
     </article>
 
 
-    <article class="">
+    <article class="my-14">
 
         <div class="flex flex-row justify-center place-items-center">
             <h2 class="text-light-blue font-title mx-3 text-4xl">Coordonnées</h2>
@@ -37,47 +41,47 @@
         </div>
 
 
-            <div class="flex xl:flex-row flex-col xl:justify-between items-center xl:mx-64 my-14" >
+            <div class="flex xl:flex-row flex-col xl:justify-around items-center xl:mx-64 my-14" >
 
                 <div class=" flex flex-col text-center xl:text-left">
-                    <div class="flex items-center mb-2">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                    <div class="flex items-center mb-2 ">
+                        <i class="fas fa-phone-square-alt fa-2x"></i>
                         <p class="text-2xl mx-4">06.00.00.00.00</p>
                     </div>
                     <div class="flex items-center mb-2 ">
-                        <img src="../img/footer/contact_email.svg" class="w-xxs">
+                        <i class="fas fa-envelope fa-2x"></i>
                         <a href="mailto: project@easyapply.fr" class="link link-underline text-2xl mx-4">
                             clementine.puntelle@exemple.com
                         </a>
                     </div>
                     <div class="flex items-center mb-2 ">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fab fa-internet-explorer fa-2x"></i>
                         <p class="text-2xl mx-4">http://www.clementine-puntelle.tk/</p>
                     </div>
                 </div>
 
                 <div class=" flex flex-col text-center xl:text-left">
                     <div class="flex items-center mb-2">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fab fa-instagram fa-2x"></i>
                         <p class="text-2xl mx-4">clém_puntelle</p>
                     </div>
                     <div class="flex items-center mb-2">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fab fa-facebook-square fa-2x"></i>
                         <p class="text-2xl mx-4">Clémentine Puntelle</p>
                     </div>
                     <div class="flex items-center mb-2 ">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fas fa-lock fa-2x"></i>
                         <p class="text-2xl mx-4">A123456*</p>
                     </div>
                 </div>
 
                 <div class=" flex flex-col text-center xl:text-left">
                     <div class="flex items-center mb-2">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fab fa-twitter-square fa-2x"></i>
                         <p class="text-2xl mx-4">clém_puntelle</p>
                     </div>
                     <div class="flex items-center mb-2">
-                        <img src="../img/footer/contact_phone.svg" class="w-xxs">
+                        <i class="fab fa-linkedin fa-2x"></i>
                         <p class="text-2xl mx-4">Clémentine Puntelle</p>
                     </div>
                 </div>
@@ -86,23 +90,21 @@
     </article>
 
 
-    <div class="flex flex-row justify-center place-items-center">
+    <div class="flex flex-row justify-center place-items-center mt-20">
         <h2 class="text-light-blue font-title mx-3 text-4xl">Formation</h2>
         <img src="../img/overwrite-icon.svg" alt="overwrite" class="w-xxs h-xxs">
     </div>
 
-    <section class="xl:flex xl:flex-row xl:justify-evenly xl:mt-24 flex flex-col justify-center place-items-center">
-        <div class="highlight shadow-md hover:shadow-xl bg-white rounded-3xl p-10 m-5 relative w-lg my-14">
+    <section class="xl:flex xl:flex-row xl:justify-evenly xl:mt-14 flex flex-col justify-center place-items-center">
+        <div class="flex flex-col items-start">
+            <label for="img" class="my-4">CV</label>
 
-            <h3 class="font-title text-light-blue text-center text-2xl font-bold mt-5">Stage en pharmacie</h3>
-            <p class="text-center">De mai 2020 à juillet 2020</p>
+            <div class="dashed-hover rounded-3xl py-20 px-20 bg-white ">
+                <input id="file-input" type="file" class="hidden" name="pdf">
+                <label for="file-input"><img width="50" height="50" class="m-auto cursor-pointer"  src="../img/add-pic.svg" alt="logo"></label>
+            </div>
 
-            <p class="text-xl mt-14">Stage rémunéré de 3 mois dans une pharmacie
-                dans le 5ème arrondissement de Paris.
-            </p><br>
-            <p class="text-xl mt-5">- approvisionnement des stocks</p>
-            <p class="text-xl mt-5">- conseil des clients</p>
-            <p class="text-xl mt-5">- vente de médicaments sur ordonnance</p>
+
         </div>
 
         <div class="highlight shadow-md hover:shadow-xl bg-white rounded-3xl p-10 m-5 relative w-lg my-14">
@@ -143,7 +145,6 @@
             <p class="text-xl mt-5">- conseil des clients</p>
             <p class="text-xl mt-5">- vente de médicaments sur ordonnance</p>
         </div>
-
 
 
     </section>
